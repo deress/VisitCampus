d8e448f4c313a7ebc3c9a4f72f233c66d36a159b
Moch Yogi Firmansyah <yogifirmansyah1407@gmail.com>
2023-12-14 19:56:55 +0700

    Resolve conflict

--- app/src/main/java/com/dicoding/visitcampus/ui/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/visitcampus/ui/main/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.dicoding.visitcampus.ui.main
 
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.dicoding.visitcampus.R
 import com.dicoding.visitcampus.databinding.ActivityMainBinding
-<<<<<<< HEAD
 import com.dicoding.visitcampus.ui.chatbot.ChatbotFragment
-=======
 import com.dicoding.visitcampus.ui.exercise.ExamListFragment
->>>>>>> exam
 import com.dicoding.visitcampus.ui.home.HomeFragment
 import com.dicoding.visitcampus.ui.university.UniversitiesFragment
 import com.google.android.material.appbar.MaterialToolbar
@@ -35,11 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         val homeFragment = HomeFragment()
         val universitiesFragment = UniversitiesFragment()
-<<<<<<< HEAD
         val chatbotFragment = ChatbotFragment()
-=======
         val examListFragment = ExamListFragment()
->>>>>>> exam
         setCurrentFragment(homeFragment)
 
         bottomNavigationView = binding.bottomNavigationView
@@ -47,11 +40,8 @@ class MainActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.home -> setCurrentFragment(homeFragment)
                 R.id.list_university -> setCurrentFragment(universitiesFragment)
-<<<<<<< HEAD
                 R.id.chatbot -> setCurrentFragment(chatbotFragment)
-=======
                 R.id.exam_practice -> setCurrentFragment(examListFragment)
->>>>>>> exam
             }
             return@setOnItemSelectedListener true
         }
